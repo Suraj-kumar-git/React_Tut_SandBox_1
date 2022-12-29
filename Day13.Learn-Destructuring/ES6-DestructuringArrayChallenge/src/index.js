a328c9c0837f0f25e2e7d5d61ed260919d3e3749
Suraj Kumar <navneetraj07903@gmail.com>
2022-12-29 17:37:06 +0530

    Destructuring Array
    Many Times we get the data in Array forms(json format) mostly from APIs. So to use them efficiently we are learning how to DeStructure Array elements.

---
+++ Day13.Learn-Destructuring/ES6-DestructuringArrayChallenge/src/index.js
@@ -0,0 +1,26 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import Practice from "./practice";
+
+
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
