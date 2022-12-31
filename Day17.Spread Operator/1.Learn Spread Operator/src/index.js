dba0c24c2e01b640690c5ac37adb52bb5e6347ef
Suraj Kumar <navneetraj07903@gmail.com>
2022-12-31 18:51:42 +0530

    Spread Operator
    Learning Spread Operator to reduce the amount of code and making code more readable.

---
+++ Day17.Spread Operator/1.Learn Spread Operator/src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+// import App2 from "./components/App2";
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App2 />, document.getElementById("root"));
+
+// const citrus = ["Lime", "Lemon", "Orange"];
+// const fruits = ["Apple", ...citrus, "Banana", "Coconut"];
+
+// const fullName = {
+//   fName: "James",
+//   lName: "Bond"
+// };
+
+// const user = {
+//   ...fullName,
+//   id: 1,
+//   username: "jamesbond007"
+// };
+
+// console.log(user);
